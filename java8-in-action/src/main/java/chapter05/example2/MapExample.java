17b95f605ec78987eca79c84f4f69154c962b66e
edfeff <396859442@qq.com>
2019-11-09 18:42:14 +0800

    映射

--- java8-in-action/src/main/java/chapter05/example2/MapExample.java
+++ java8-in-action/src/main/java/chapter05/example2/MapExample.java
@@ -1,9 +1,27 @@
 package chapter05.example2;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 /**
  * 映射
  *
  * @author wangpp
  */
 public class MapExample {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(-1, -2, -1, 3, 3, 2, 4);
+        //映射出绝对值
+        numbers.stream().map(Math::abs).collect(Collectors.toList()).forEach(System.out::println);
+
+        List<String> strings = Arrays.asList("abc", "aaaa", "deafgeg");
+        //找出最长的
+        strings.stream().map(String::length).sorted(Comparator.comparing(Integer::intValue).reversed()).limit(1).forEach(System.out::println);
+
+
+    }
+
 }
