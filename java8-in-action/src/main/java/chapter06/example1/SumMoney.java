2a44bd8863e4b87eab38a3a3bc371d9324c0c4ae
edfeff <396859442@qq.com>
2019-11-11 21:30:17 +0800

    * Collectors.summingInt

--- java8-in-action/src/main/java/chapter06/example1/SumMoney.java
+++ java8-in-action/src/main/java/chapter06/example1/SumMoney.java
@@ -3,24 +3,21 @@ package chapter06.example1;
 import chapter06.pojo.Transaction;
 
 import java.util.IntSummaryStatistics;
-import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
- * Collectors.summarizingInt
+ * Collectors.summingInt
  *
  * @author wangpp
  */
 public class SumMoney {
     public static void main(String[] args) {
-        IntSummaryStatistics collect = Stream.generate(() -> new Transaction(Math.random() > 0.5 ? Transaction.Currency.Rmb : Transaction.Currency.Dollar, (int) (100 * Math.random()))).limit(10)
-                .collect(Collectors.summarizingInt(Transaction::getCount));
-        System.out.println("average:" + collect.getAverage());
-        System.out.println("count:" + collect.getCount());
-        System.out.println("max:" + collect.getMax());
-        System.out.println("min:" + collect.getMin());
-        System.out.println("sum:" + collect.getSum());
+        Integer sum = Stream.generate(() -> new Transaction(Math.random() > 0.5 ? Transaction.Currency.Rmb : Transaction.Currency.Dollar, (int) (100 * Math.random())))
+                .limit(10)
+                .collect(Collectors.summingInt(Transaction::getCount));
+        System.out.println(sum);
+
 
     }
 }
---
+++ java8-in-action/src/main/java/chapter06/example1/SummarizingMoney.java
@@ -0,0 +1,26 @@
+package chapter06.example1;
+
+import chapter06.pojo.Transaction;
+
+import java.util.IntSummaryStatistics;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * Collectors.summarizingInt
+ *
+ * @author wangpp
+ */
+public class SummarizingMoney {
+    public static void main(String[] args) {
+        IntSummaryStatistics collect = Stream.generate(() -> new Transaction(Math.random() > 0.5 ? Transaction.Currency.Rmb : Transaction.Currency.Dollar, (int) (100 * Math.random()))).limit(10)
+                .collect(Collectors.summarizingInt(Transaction::getCount));
+        System.out.println("average:" + collect.getAverage());
+        System.out.println("count:" + collect.getCount());
+        System.out.println("max:" + collect.getMax());
+        System.out.println("min:" + collect.getMin());
+        System.out.println("sum:" + collect.getSum());
+
+    }
+}
