8e94839dc0dc9b89abc076b623de6c627edeafd4
edfeff <396859442@qq.com>
2019-11-20 22:07:50 +0800

    add 自定义登陆页面 自定义登陆url

---
+++ security-parent/springsecurity_1/src/main/java/com/wpp/controller/MainController.java
@@ -0,0 +1,17 @@
+package com.wpp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author wangpp
+ */
+@Controller
+public class MainController {
+    @RequestMapping( "/userLogin" )
+    public String login() {
+        return "login";
+    }
+}
---
+++ security-parent/springsecurity_1/src/main/webapp/WEB-INF/jsp/login.jsp
@@ -0,0 +1,11 @@
+<html>
+<body>
+<h2>login</h2>
+<%--<form action="${pageContext.request.contextPath}/login" method="post">--%>
+<form action="${pageContext.request.contextPath}/come" method="post">
+    user:<input type="text" name="username"> <br>
+    pass:<input type="password" name="password"> <br>
+    <input type="submit" value="LOGIN">
+</form>
+</body>
+</html>
