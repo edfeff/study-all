48a3e0687a724e8815894929f6e245ebf08534ec
edfeff <396859442@qq.com>
2019-11-20 22:24:21 +0800

    add 角色与资源的访问限制 add 自定义错误页面

--- security-parent/springsecurity_1/src/main/java/com/wpp/controller/MainController.java
+++ security-parent/springsecurity_1/src/main/java/com/wpp/controller/MainController.java
@@ -14,4 +14,9 @@ public class MainController {
     public String login() {
         return "login";
     }
+
+    @RequestMapping( "/error" )
+    public String error() {
+        return "error";
+    }
 }
